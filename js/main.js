64f93d6ca56747493d29b428a3bae80660972cff
Kevin Mangubat <kevin.mangu@gmail.com>
2013-06-22 12:13:59 -0400

    adding test page that responds to query strings

--- js/main.js
+++ js/main.js
@@ -1,14 +1,50 @@
-!function () {
+!function() {
+	/* ========================================
+		Namespace: Global Variables
+	======================================== */
+	var G = {};
+	!function(gv) {
+		// place global variables here
+	}(G);
+
 	/**
-	 *	get a specific paramter from URL
+	 *	Append query on click
 	 **/
-	var getParam = function(param) {
-		// set regex to look for ?query=value or &query=value, case insensitive
+	!function() {
+		var playground = document.getElementById('playground'),
+			locThere = document.querySelector('a.there'),
+			query = '?location=there';
+
+		if(locThere) {
+			// stop default function and fire new href with query params
+			locThere.addEventListener('click', function(e) {
+				e.preventDefault();
+				var qHref = this.attributes[0].value + query;
+				document.location.href = qHref;
+			}, false);
+			console.log(locThere);
+		}
+	}();
+	/**
+	 *	Get a specific paramter from URL
+	 **/
+	var getParam = function(name) {
+		// set regex to look for ?x=y or &x=y, case insensitive
 		// tests the current URL against the regex
-		var regx = new RegExp('[?&]' + param + '=([^?|^&]*)', 'i'),
-			querify = regx.exec(window.location.href);		
-		// returns only the value of called parameter
-		console.log(querify[1]);
-		return querify[1];
-	};
+		var regx = new RegExp('[?&]' + name + '=([^?|^&]*)', 'i'),
+			result = regx.exec(window.location.href);		
+		// returns only the param
+		if(result) {
+			console.log(result[1]);
+			return result[1];
+		}
+	}
+	/**
+	 *	Lets try some fun stuff base on some params
+	 **/
+	if(getParam('location')) {
+		var locText = document.querySelector('b[data="location"]');
+		locText.innerText = 'there';
+		console.log(locText);
+	}
 }();
